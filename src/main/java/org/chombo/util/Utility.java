a758716db945b02279bb1d99369672a3f433ee20
Pranab Ghosh <pkghosh99@gmail.com>
2012-04-15 18:48:52 -0700

    default FS and HDFS location for config parameters

--- src/main/java/org/chombo/util/Utility.java
+++ src/main/java/org/chombo/util/Utility.java
@@ -19,14 +19,26 @@
 package org.chombo.util;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
 public class Utility {
+	private static final String CONF_FILE_PROP_NAME = "conf.path";
+	private static final String FS_DEF_CONFIG_DIR = "/var/mawazo/";
+	private static final String HDFS_DEF_CONFIG_DIR = "/var/mawazo/";
+	private static final String HDFS_PREFIX = "hdfs:";
+	private static final int HDFS_PREFIX_LEN = 5;
+	private static final String PROP_FILE_EXT = ".properties";
+	
     public static void setConfiguration(Configuration conf) throws Exception{
         String confFilePath = conf.get("conf.path");
         if (null != confFilePath){
@@ -40,6 +52,72 @@ public class Utility {
             }
         }
     }
+
+    public static void setConfiguration(Configuration conf, String project) throws Exception{
+        boolean found = false;
+    	String confFilePath = conf.get(CONF_FILE_PROP_NAME);
+    	
+    	//user provided config file path
+        if (null != confFilePath){
+        	if (confFilePath.startsWith(HDFS_PREFIX)) {
+		        loadConfigHdfs( conf,  confFilePath.substring(HDFS_PREFIX_LEN));
+		        System.out.println("config found in user specified HDFS file");
+        	} else {
+        		loadConfig( conf,  confFilePath, false);
+		        System.out.println("config found in user specified FS  file");
+        	}
+         } else {
+	        //default file system path
+	        confFilePath = FS_DEF_CONFIG_DIR + project + PROP_FILE_EXT;
+	        found = loadConfig( conf,  confFilePath, true);
+	        
+	        //default HDFS path
+	        if (!found) {
+		        confFilePath = HDFS_DEF_CONFIG_DIR + project + PROP_FILE_EXT;
+		        loadConfigHdfs( conf,  confFilePath);
+		        System.out.println("config found in default HDFS location");
+	        }  else {
+		        System.out.println("config found in default FS location");
+	        }
+         }
+    }
+    
+   private static boolean loadConfig(Configuration conf, String confFilePath, boolean handleErr ) throws IOException {
+	   boolean found = false;
+	   try {
+	        FileInputStream fis = new FileInputStream(confFilePath);
+	        Properties configProps = new Properties();
+	        configProps.load(fis);
+	
+	        for (Object key : configProps.keySet()){
+	            String keySt = key.toString();
+	            conf.set(keySt, configProps.getProperty(keySt));
+	        }
+	        found = true;
+	   } catch (FileNotFoundException ex) {
+		   if (!handleErr) {
+			   throw ex;
+		   }
+	   }
+	   return found;
+   }
+   
+   private static boolean loadConfigHdfs(Configuration conf, String confFilePath) throws IOException {
+	   boolean found = false;
+
+	   FileSystem dfs = FileSystem.get(conf);
+       Path src = new Path(confFilePath);
+       FSDataInputStream fis = dfs.open(src);
+       Properties configProps = new Properties();
+       configProps.load(fis);
+
+       for (Object key : configProps.keySet()){
+           String keySt = key.toString();
+           conf.set(keySt, configProps.getProperty(keySt));
+       }
+       found = true;
+       return found;
+   }
 
     public static <T> void initializeArray(T[] vec, T val)  {
     	for(int i = 0; i < vec.length; ++i) {
