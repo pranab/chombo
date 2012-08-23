0a2cadc2a848d5a9102ff3570797f3ddad4d223e
Pranab Ghosh <pkghosh99@gmail.com>
2012-08-22 23:40:42 -0700

    default constructor

--- src/main/java/org/chombo/util/Pair.java
+++ src/main/java/org/chombo/util/Pair.java
@@ -29,6 +29,9 @@ public class Pair<L,R>  {
 	  private  L left;
 	  private  R right;
 
+	  public Pair() {
+	  }
+	  
 	  public Pair(L left, R right) {
 	    this.left = left;
 	    this.right = right;
