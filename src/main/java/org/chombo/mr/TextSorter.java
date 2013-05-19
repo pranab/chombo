fa08eef8687a3c0b95a6b8784c1c03c3d8c465c7
Pranab Ghosh <pkghosh99@gmail.com>
2013-05-18 22:46:24 -0700

    sorting by text field

---
+++ src/main/java/org/chombo/mr/TextSorter.java
@@ -0,0 +1,114 @@
+/*
+ * chombo: Hadoop Map Reduce utility
+ * Author: Pranab Ghosh
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.chombo.mr;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.chombo.util.Utility;
+
+/**
+ * @author pranab
+ *
+ */
+public class TextSorter  extends Configured implements Tool {
+
+	@Override
+	public int run(String[] args) throws Exception {
+        Job job = new Job(getConf());
+        String jobName = "Sorting by text field MR";
+        job.setJobName(jobName);
+        
+        job.setJarByClass(TextSorter.class);
+        
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        
+        job.setMapperClass(TextSorter.SorterMapper.class);
+        job.setReducerClass(TextSorter.SorterReducer.class);
+        
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(Text.class);
+
+        job.setOutputKeyClass(NullWritable.class);
+        job.setOutputValueClass(Text.class);
+  
+        Utility.setConfiguration(job.getConfiguration());
+        job.setNumReduceTasks(job.getConfiguration().getInt("num.reducer", 1));
+        
+        int status =  job.waitForCompletion(true) ? 0 : 1;
+        return status;
+	}
+	
+	public static class SorterMapper extends Mapper<LongWritable, Text, Text, Text> {
+		private Text outKey = new Text();
+		private int sortField;
+        private String fieldDelimRegex;
+        private boolean sortOrderAscending;
+        private String  sortFieldVal;
+        
+        protected void setup(Context context) throws IOException, InterruptedException {
+        	sortField = context.getConfiguration().getInt("sort.field", 0);
+        	sortOrderAscending = context.getConfiguration().getBoolean("sort.order.ascending", true);
+        	fieldDelimRegex = context.getConfiguration().get("field.delim.regex", ",");
+       }
+
+        @Override
+        protected void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+            String[] items  =  value.toString().split(fieldDelimRegex);
+            
+            sortFieldVal = items[sortField];
+            if (!sortOrderAscending) {
+            }
+            
+        	outKey.set(sortFieldVal);
+			context.write(outKey, value);
+        }
+	}
+	
+    public static class SorterReducer extends Reducer<Text, Text, NullWritable, Text> {
+    	
+    	protected void reduce(Text key, Iterable<Text> values, Context context)
+        	throws IOException, InterruptedException {
+        	for (Text value : values){
+ 				context.write(NullWritable.get(), value);
+        	}    		
+    	}
+    	
+    }
+ 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new TextSorter(), args);
+        System.exit(exitCode);
+	}
+}
