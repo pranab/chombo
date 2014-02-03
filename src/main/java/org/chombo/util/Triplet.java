e0fc096ca9c3f2e8e3b512d7d2d1a37103439819
Pranab Ghosh <pkghosh99@gmail.com>
2014-02-03 04:59:18 -0800

    triplet class

--- src/main/java/org/chombo/util/Triplet.java
+++ src/main/java/org/chombo/util/Triplet.java
@@ -15,56 +15,49 @@
  * permissions and limitations under the License.
  */
 
-
 package org.chombo.util;
 
 /**
- * Generic Pair class
+ * Triplet generic
  * @author pranab
  *
  * @param <L>
+ * @param <C>
  * @param <R>
  */
-public class Pair<L,R>  {
-	  protected  L left;
-	  protected  R right;
+public class Triplet<L, C, R> extends Pair<L,R> {
+	private C center;
 
-	  public Pair() {
-	  }
-	  
-	  public Pair(L left, R right) {
-	    this.left = left;
-	    this.right = right;
-	  }
-
-	  public L getLeft() {
-		return left;
+	public Triplet() {
+		super();
 	}
 
-	public void setLeft(L left) {
-		this.left = left;
+	public Triplet(L left, C center, R right) {
+		super(left, right);
+		this.center = center;
 	}
 
-	public R getRight() {
-		return right;
+	public C getCenter() {
+		return center;
 	}
 
-	public void setRight(R right) {
-		this.right = right;
+	public void setCenter(C center) {
+		this.center = center;
 	}
 
 	@Override
 	public int hashCode() { 
-		return left.hashCode() ^ right.hashCode();
+		return left.hashCode() ^ center.hashCode() ^ right.hashCode();
 	}
 
 	  @Override
 	  public boolean equals(Object other) {
 		  boolean isEqual = false;
-		  if (null != other && other instanceof Pair) {
-			    Pair pairOther = (Pair) other;
-			    isEqual =  this.left.equals(pairOther.left) &&
-			           this.right.equals(pairOther.right); 
+		  if (null != other && other instanceof Triplet) {
+			  Triplet tripleOther = (Triplet) other;
+			    isEqual =  this.left.equals(tripleOther.left) &&
+			    		this.center.equals(tripleOther.center) &&
+			    		this.right.equals(tripleOther.right); 
 		  }
 		   return isEqual;
 	  }
