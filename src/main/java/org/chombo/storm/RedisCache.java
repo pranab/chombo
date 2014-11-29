4b1bed85013ea5397337a2edeeb5b962df50e90c
Pranab Ghosh <pkghosh99@gmail.com>
2014-11-29 15:38:01 -0800

    default cache

--- src/main/java/org/chombo/storm/RedisCache.java
+++ src/main/java/org/chombo/storm/RedisCache.java
@@ -18,21 +18,43 @@ public class RedisCache extends Cache {
 	 * @param conf
 	 * @param queue
 	 */
+	public RedisCache(Map conf) {
+		String redisHost = ConfigUtility.getString(conf, "redis.server.host");
+		int redisPort = ConfigUtility.getInt(conf,"redis.server.port");
+		jedis = new Jedis(redisHost, redisPort);
+	}
+	
+	/**
+	 * @param conf
+	 * @param queue
+	 */
 	public RedisCache(Map conf, String cacheName) {
 		String redisHost = ConfigUtility.getString(conf, "redis.server.host");
 		int redisPort = ConfigUtility.getInt(conf,"redis.server.port");
 		jedis = new Jedis(redisHost, redisPort);
-		this.cacheName = cacheName;
+		if (null != cacheName) {
+			this.cacheName = cacheName;
+		}
 	}
 
 	@Override
 	public void set(String key, String value) {
-		jedis.hset(cacheName, key, value);	
+		if (null != cacheName) {
+			jedis.hset(cacheName, key, value);	
+		} else {
+			jedis.set(key, value);
+		}
 	}
 
 	@Override
 	public String get(String key) {
-		return jedis.hget(cacheName, key);
+		String value = null;
+		if (null != cacheName) {
+			value = jedis.hget(cacheName, key);
+		} else {
+			value = jedis.get(key);
+		}
+		return value;
 	}
 
 }
