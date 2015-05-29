e9fae2d4a1a64208e555d31efb2f5e40445943e6
Pranab Ghosh <pkghosh99@gmail.com>
2015-05-29 09:20:42 -0700

    data validation

---
+++ src/main/java/org/chombo/validator/StringValidator.java
@@ -0,0 +1,99 @@
+/*
+ * chombo: Hadoop Map Reduce utility
+ * Author: Pranab Ghosh
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.chombo.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.chombo.util.AttributeSchema;
+
+/**
+ * @author pranab
+ *
+ */
+public class StringValidator {
+
+	/**
+	 * @author pranab
+	 *
+	 */
+	public static class MinLengthValidator extends Validator {
+
+		public MinLengthValidator(String tag, int ordinal, AttributeSchema schema) {
+			super(tag, ordinal, schema);
+		}
+
+		@Override
+		public boolean isValid(String value) {
+			return value.length() >= attribute.getMin();
+		}
+	}
+
+	/**
+	 * @author pranab
+	 *
+	 */
+	public static class MaxLengthValidator extends Validator {
+
+		public MaxLengthValidator(String tag, int ordinal, AttributeSchema schema) {
+			super(tag, ordinal, schema);
+		}
+
+		@Override
+		public boolean isValid(String value) {
+			return value.length() <= attribute.getMax();
+		}
+	}
+
+	/**
+	 * @author pranab
+	 *
+	 */
+	public static class NotMissingValidator extends Validator {
+		
+		public NotMissingValidator(String tag, int ordinal, AttributeSchema schema) {
+			super(tag, ordinal, schema);
+		}
+
+		@Override
+		public boolean isValid(String value) {
+			return !value.isEmpty();
+		}
+	}
+	
+	/**
+	 * @author pranab
+	 *
+	 */
+	public static class PatternValidator extends Validator {
+		private Pattern pattern;
+		
+		public PatternValidator(String tag, int ordinal, AttributeSchema schema) {
+			super(tag, ordinal, schema);
+		}
+
+		@Override
+		public boolean isValid(String value) {
+			if (null == pattern) {
+				pattern = Pattern.compile(attribute.getStringPattern());
+			}
+			Matcher matcher = pattern.matcher(value);
+			return matcher.matches();
+		}
+	}
+}
---
+++ src/main/java/org/chombo/validator/Validator.java
@@ -0,0 +1,55 @@
+/*
+ * chombo: Hadoop Map Reduce utility
+ * Author: Pranab Ghosh
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package org.chombo.validator;
+
+import org.chombo.util.Attribute;
+import org.chombo.util.AttributeSchema;
+
+/**
+ * @author pranab
+ *
+ */
+public abstract class Validator {
+	protected String tag;
+	protected int ordinal;
+	protected Attribute attribute;
+	
+	
+	/**
+	 * @param tag
+	 * @param ordinal
+	 * @param schema
+	 */
+	public Validator(String tag, int ordinal, AttributeSchema schema) {
+		super();
+		this.tag = tag;
+		this.ordinal = ordinal;
+		attribute = schema.findAttributeByOrdinal(ordinal);
+	}
+	
+	public abstract boolean isValid(String value);
+
+	public String getTag() {
+		return tag;
+	}
+
+	public int getOrdinal() {
+		return ordinal;
+	}
+	
+}
