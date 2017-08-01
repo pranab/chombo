fb565574bd5970f10e45b5f08b7d6fcd196345c0
Pranab Ghosh <pkghosh99@gmail.com>
2017-07-31 20:28:45 -0700

    add KL divergence to distr

--- spark/src/main/scala/org/chombo/spark/explore/NumericalAttrDistrStats.scala
+++ spark/src/main/scala/org/chombo/spark/explore/NumericalAttrDistrStats.scala
@@ -23,6 +23,9 @@ import org.apache.spark.SparkContext
 import scala.collection.JavaConverters._
 import org.chombo.spark.common.Record
 import org.chombo.stats.HistogramStat
+import org.chombo.stats.HistogramUtility
+import java.io.FileInputStream
+import scala.collection.mutable.Map
 
 object NumericalAttrDistrStats extends JobConfiguration {
   
@@ -41,7 +44,17 @@ object NumericalAttrDistrStats extends JobConfiguration {
 	   //configurations
 	   val fieldDelimIn = getStringParamOrElse(appConfig, "field.delim.in", ",")
 	   val fieldDelimOut = getStringParamOrElse(appConfig, "field.delim.out", ",")
-	   val keyFieldOrdinals = getMandatoryIntListParam(appConfig, "id.field.ordinals").asScala.toArray
+	   val keyFields = getOptionalIntListParam(appConfig, "id.field.ordinals")
+	   val keyFieldOrdinals = keyFields match {
+	     case Some(fields:java.util.List[Integer]) => Some(fields.asScala.toArray)
+	     case None => None  
+	   }
+	   val keyLen = keyFieldOrdinals match {
+		     case Some(fields:Array[Integer]) => fields.length + 1
+		     case None =>1
+	   }
+
+	   //val keyFieldOrdinals = getMandatoryIntListParam(appConfig, "id.field.ordinals").asScala.toArray
 	   val numAttrOrdinals = getMandatoryIntListParam(appConfig, "num.attr.ordinals", "").asScala.toArray
 	   val binWidths = numAttrOrdinals.map(ord => {
 	     //attribute bin width tuple
@@ -50,6 +63,8 @@ object NumericalAttrDistrStats extends JobConfiguration {
 	   })
 	   val extendedOutput = getBooleanParamOrElse(appConfig, "extended.output", true)
 	   val outputPrecision = getIntParamOrElse(appConfig, "output.precision", 3);
+	   val refDistrFilePath = getOptionalStringParam(appConfig, "reference.distr.file.path")
+	   
 	   val debugOn = getBooleanParamOrElse(appConfig, "debug.on", false)
 	   val saveOutput = getBooleanParamOrElse(appConfig, "save.output", true)
 	   
@@ -58,7 +73,11 @@ object NumericalAttrDistrStats extends JobConfiguration {
 	   //key with record key and attr ordinal and value map of counts
 	   val pairedData = data.flatMap(line => {
 		   val items = line.split(fieldDelimIn, -1)
-		   val keyRec = Record(items, keyFieldOrdinals)
+		   //val keyRec = Record(items, keyFieldOrdinals)
+		   val keyRec = keyFieldOrdinals match {
+		     case Some(fields:Array[Integer]) => Record(keyLen, items, fields)
+		     case None => Record(keyLen)
+		   }
 		   val attrValCount = binWidths.map(ord => {
 		     val attrKeyRec = Record(keyRec.size + 1 ,keyRec)
 		     attrKeyRec.addInt(ord._1)
@@ -80,16 +99,45 @@ object NumericalAttrDistrStats extends JobConfiguration {
 	   
 	   //merge histograms
 	   val stats = pairedData.reduceByKey((h1, h2) => h1.merge(h2))
+	   val colStats = stats.collect
+	   
+	   //reference stats
+	   val refStats = refDistrFilePath match {
+	     case Some(path:String) => {
+	       val refStats = HistogramUtility.createHiostograms(new FileInputStream(path), keyLen, true)
+	       val stats = refStats.asScala.map(kv => {
+	         (Record(kv._1), kv._2)
+	       })
+	       Some(stats)
+	     }
+	     case None => None
+	   }
 
+	   //append KL divergence
+	   val modStats = colStats.map(v => {
+	     val stat = refStats match {
+	       case Some(stats:Map[Record,HistogramStat]) => {
+	         val key = v._1
+	         val refDistr = stats.get(key).get
+	         val thidDistr = v._2
+	         val diverge = HistogramUtility.findKullbackLeiblerDivergence(refDistr, thidDistr)
+	         (v._1, v._2, diverge)
+	       }
+	       case None => (v._1, v._2, 0.0)
+	     }
+	     stat
+	   })
+	   
 	   if (debugOn) {
-	     val colStats = stats.collect
-	     colStats.foreach(s => {
+	     modStats.foreach(s => {
 	       println("id:" + s._1)
 	       println("distr:" + s._2)
+	       println("dvergence:" + s._3)
 	     })
 	   }
 	   
 	   if (saveOutput) {
+	     val stats = sparkCntxt.parallelize(modStats)
 	     stats.saveAsTextFile(outputPath)
 	   }
 	   
