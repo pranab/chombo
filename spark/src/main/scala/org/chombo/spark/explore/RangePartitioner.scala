59300ec987b45472e24e05e9e1b45d4a6b08a8a0
Pranab Ghosh <pkghosh99@gmail.com>
2018-12-02 20:33:25 -0800

    range partitioning by number of partitons

--- spark/src/main/scala/org/chombo/spark/explore/RangePartitioner.scala
+++ spark/src/main/scala/org/chombo/spark/explore/RangePartitioner.scala
@@ -22,6 +22,7 @@ import org.apache.spark.SparkContext
 import scala.collection.JavaConverters._
 import org.chombo.spark.common.Record
 import org.chombo.util.BasicUtils
+import java.text.SimpleDateFormat
 
 /**
  * Range partitions. Adds new field with partition index
@@ -35,7 +36,7 @@ object RangePartitioner extends JobConfiguration  {
     * @return
     */
    def main(args: Array[String]) {
-	   val appName = "numericalAttrStats"
+	   val appName = "rangePartitioner"
 	   val Array(inputPath: String, outputPath: String, configFile: String) = getCommandLineArgs(args, 3)
 	   val config = createConfig(configFile)
 	   val sparkConf = createSparkConf(appName, config, false)
@@ -45,33 +46,141 @@ object RangePartitioner extends JobConfiguration  {
 	   //configurations
 	   val fieldDelimIn = getStringParamOrElse(appConfig, "field.delim.in", ",")
 	   val fieldDelimOut = getStringParamOrElse(appConfig, "field.delim.out", ",")
+	   val keyFields = getOptionalIntListParam(appConfig, "id.fieldOrdinals")
+	   val keyFieldOrdinals = keyFields match {
+	     case Some(fields:java.util.List[Integer]) => Some(fields.asScala.toArray)
+	     case None => None  
+	   }
+	   
 	   val seqFieldOrdinal = getMandatoryIntParam(appConfig, "seq.fieldOrdinal", 
 	       "missing seq field ordinal")
-	   val rangeBoundaries = getMandatoryDoubleListParam(appConfig, "range.boundaries", "missing range definitions").
-	   		asScala.toList.zipWithIndex
-	   
-	  val debugOn = getBooleanParamOrElse(appConfig, "debug.on", false)
-	  val saveOutput = getBooleanParamOrElse(appConfig, "save.output", true)
-	  
-	  //input
-	  val data = sparkCntxt.textFile(inputPath)	  
-	  val partData = data.map(line => {
-	    val fields = BasicUtils.getTrimmedFields(line, fieldDelimIn)
-	    val seq = fields(seqFieldOrdinal).toDouble
-	    val range = rangeBoundaries.find(r => seq < r._1)
-	    range match {
-	      case Some(r) => "" + r._2 + fieldDelimOut + line
-	      case None => "" + rangeBoundaries.length + fieldDelimOut + line
-	    }
-	  }) 
+	   val numPartitions = getOptionalIntParam(appConfig, "num.partitions")
+	   val dateFormatStr = getOptionalStringParam(appConfig, "date.formatStr")
+	   val dateFormat = dateFormatStr match {
+	     case Some(date) => Some(new SimpleDateFormat(date))
+	     case None => None
+	   }
+	   val debugOn = getBooleanParamOrElse(appConfig, "debug.on", false)
+	   val saveOutput = getBooleanParamOrElse(appConfig, "save.output", true)
 
-	  if (debugOn) {
-	     partData.collect.slice(0,50).foreach(s => println(s))
-	  }
+	   var keyLen = 0
+	   keyFieldOrdinals match {
+	     case Some(fields : Array[Integer]) => keyLen +=  fields.length
+	     case None => 
+	   }
+
+	   //input
+	   val data = sparkCntxt.textFile(inputPath)
 	   
-	  if (saveOutput) {
+	   val partData = numPartitions match {
+	     case Some(numPart) => {
+	       //key wise or global based on number of partitions
+	       val minRange = getMandatoryDoubleParam(appConfig, "range.min", "missing min range")
+		   data.cache
+		   
+	       //min max
+		   val dataRange = data.map(line => {
+		    val fields = BasicUtils.getTrimmedFields(line, fieldDelimIn)
+		    val seq = getPartField(fields(seqFieldOrdinal), dateFormat)
+		    val key = buildKey(keyLen, fields, keyFieldOrdinals)
+		    (key, (seq, seq))
+		   }).reduceByKey((v1, v2) => {
+		     val min = if (v1._1 < v2._1) v1._1 else v2._1
+		     val max = if (v1._2 > v2._2) v1._2 else v2._2
+		     (min, max)
+		   })
+		   
+		   //partition size
+		   val partSize = dataRange.mapValues(r => {
+		     val range = r._2 - r._1
+		     if (range > minRange) {
+		       //normal
+		       val size = range / numPart
+		       (r._1, r._2, size, true)
+		     } else {
+		       // range too small
+		       (r._1, r._2, range, false)
+		     }
+		   }).collectAsMap
+		   
+		   //partition
+		   val partData = data.map(line => {
+		     val fields = BasicUtils.getTrimmedFields(line, fieldDelimIn)
+		     val seq = getPartField(fields(seqFieldOrdinal), dateFormat)
+		     val key = buildKey(keyLen, fields, keyFieldOrdinals)
+		     val ptSize = partSize.get(key)
+		     val ptIndex = ptSize match {
+		       case Some(part) => {
+		         if (part._4) {
+		           var ptIndex = (seq / part._3).floor.toInt
+		           ptIndex = BasicUtils.between(ptIndex, 0, numPart - 1)
+		           ptIndex
+		         } else {
+		           0
+		         }
+		       }
+		       case None => throw new IllegalStateException("missing partition information")
+		     }
+		     ptIndex.toString + fieldDelimOut + line
+		   }) 
+		   partData
+	     }
+	   
+	     case None => {
+	       //global based on specified partition boundaries
+	       val partBoundaries = getMandatoryDoubleListParam(appConfig, "part.boundaries", "missing range definitions").
+	           asScala.toList.zipWithIndex
+		   val partData = data.map(line => {
+		    val fields = BasicUtils.getTrimmedFields(line, fieldDelimIn)
+		    val seq = getPartField(fields(seqFieldOrdinal), dateFormat)
+		    val range = partBoundaries.find(r => seq < r._1)
+		    range match {
+		      case Some(r) => r._2.toString + fieldDelimOut + line
+		      case None => partBoundaries.length.toString + fieldDelimOut + line
+		    }
+		   }) 
+		   partData
+	     }
+	   }
+	   
+	   if (debugOn) {
+	     partData.collect.slice(0,50).foreach(s => println(s))
+	   }
+	   
+	   if (saveOutput) {
 	     partData.saveAsTextFile(outputPath)
-	  }
+	   }
 	  
+   }
+   
+   /**
+   * @param config
+   * @param paramName
+   * @param defValue
+   * @param errorMsg
+   * @return
+   */   
+   def buildKey( keyLen:Int, fields:Array[String], keyFieldOrdinals: Option[Array[Integer]]) : Record = {
+      if (keyLen > 0) {
+	      val key = Record(keyLen)
+		  Record.populateFields(fields, keyFieldOrdinals, key)
+	      key
+	  } else {
+	      val key = Record(1)
+	      key.add("fake")
+	      key
+	  }
+   }
+   
+   /**
+   * @param value
+   * @param dateFormat
+   * @return
+   */
+   def getPartField(value:String, dateFormat: Option[java.text.SimpleDateFormat]) : Double = {
+     dateFormat match {
+       case Some(dateFmt) => BasicUtils.getEpochTime(value, dateFmt)
+       case None => value.toDouble
+     }
    }
 }
